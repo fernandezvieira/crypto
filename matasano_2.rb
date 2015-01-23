201b80eec8204fd0c296536b217421e41df7df05
Daniel Fernandez <danfernandezv@gmail.com>
2015-01-23 14:19:26 +0000

    Challenge 6 WIP.

--- matasano_2.rb
+++ matasano_2.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/ruby
 
+require 'base64'
+
 def hex_to_base64(str)
   [[str].pack("H*")].pack("m0")
 end
@@ -23,12 +25,16 @@ def xor_bytes(bytes, bytes_2)
   bytes.zip(bytes_2).map { |l,r| l^r }
 end
 
-def xor(str1, str2)
+def xor_str(str1, str2)
   xor_bytes(str1.bytes, str2.bytes)
 end
 
 def repeating_key_xor(string, repeating_key)
   xor_bytes(string.bytes, repeating_key.bytes.cycle)
+end
+
+def repeating_key_xor_bytes(bytes, repeating_bytes)
+  xor_bytes(bytes, repeating_bytes.cycle)
 end
 
 def challenge_2
@@ -128,8 +134,117 @@ def challenge_5
   print_result(result, 5)
 end
 
-def challenge_6
+def hamming_distance(str1, str2)
+  xord = xor_str(str1, str2)
 
+  xord.inject(0) {|sum, x| sum += x.to_s(2).count("1"); sum}
+end
+
+def hamming_distance_bytes(bytes1, bytes2)
+  xord = xor_bytes(bytes1, bytes2)
+
+  xord.inject(0) {|sum, x| sum += x.to_s(2).count("1"); sum}
+end
+
+def freq(file)
+  text = File.read(file)
+  text = text.upcase
+  text = text.gsub(/\n/, ' ')
+  text = text.gsub(/ +/, ' ')
+  normalized_freq(text.bytes)
+end
+
+def normalized_freq(bytes)
+  normalize(byte_freq(bytes))
+end
+
+def normalize(freq)
+  total = freq.reduce(0) {|acc, e| acc + e}
+  freq.map {|x| x / total.to_f}
+end
+
+def byte_freq(bytes)
+  vector = [0] * 256
+  bytes.each do |x|
+    vector[x] = vector[x] + 1
+  end
+  vector
+end
+
+def unhex(hex)
+  hex.scan(/../).map {|x| x.to_i(16)}
+end
+
+def xor(bytes, single_byte)
+  bytes.map {|x| x ^ single_byte}
+end
+
+def score(actual, expected)
+  distance(normalized_freq(actual.pack("c*").upcase.bytes), expected)
+end
+
+def distance(l, r)
+  l.zip(r).reduce(0) do |acc, e|
+    diff = (e[0] - e[1])
+    acc + diff * diff
+  end
+end
+
+def challenge_6
+  expected = freq("alice_in_wonderland.txt")
+
+  file_content = Base64.decode64(File.read("6.txt"))
+
+  keysizes = [*2..40]
+
+  candidates = keysizes.map do |keysize|
+    normlised_distance = (0..5).reduce(0) do |mem, n|
+
+      first_keysize = file_content[0+n..keysize+n]
+      second_keysize = file_content[keysize+n..keysize+keysize+n]
+
+      mem += hamming_distance(first_keysize, second_keysize)/keysize
+      mem
+    end
+
+    [normlised_distance, keysize]
+  end
+
+  candidates.sort!
+
+  puts candidates.inspect
+
+  candidates.sort.each do |distance, keysize|
+    text_blocks = file_content.bytes.each_slice(keysize).map { |x| x }
+
+    transposed = keysize.times.map do |key_idx|
+      text_blocks.size.times.map do |block_idx|
+
+        text_blocks[block_idx][key_idx]
+      end.compact # need to remove any nils
+    end
+
+    solved = transposed.map do |block|
+      best = 0.0
+      key = nil
+
+      (0..255).map {|char|
+        ptext = xor(block, char)
+        current_score = score(ptext, expected)
+        if current_score > best
+          best = current_score
+          key = char
+        end
+      }
+
+      key
+    end
+
+    res = repeating_key_xor_bytes(file_content.bytes, solved).pack("c*")
+    puts res.inspect
+
+    break
+  end
 end
 
 # challenge_1
@@ -137,4 +252,4 @@ end
 # challenge_3
 # challenge_4
 # challenge_5
-# challenge_6
+challenge_6
